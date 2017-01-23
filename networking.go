b8ccb4c564207e537adbb7a308ff751935695a63
christophberger <christophberger@users.noreply.github.com>
2017-01-23 09:37:05 +0100

    Initial commit.

---
+++ networking.go
@@ -0,0 +1,40 @@
+/*
+<!--
+Copyright (c) 2016 Christoph Berger. Some rights reserved.
+Use of this text is governed by a Creative Commons Attribution Non-Commercial
+Share-Alike License that can be found in the LICENSE.txt file.
+
+The source code contained in this file may import third-party source code
+whose licenses are provided in the respective license files.
+-->
+
+<!--
+NOTE: The comments in this file are NOT godoc compliant. This is not an oversight.
+
+Comments and code in this file are used for describing and explaining a particular topic to the reader. While this file is a syntactically valid Go source file, its main purpose is to get converted into a blog article. The comments were created for learning and not for code documentation.
+-->
+
++++
+title = ""
+description = ""
+author = "Christoph Berger"
+email = "chris@appliedgo.net"
+date = "2016-00-00"
+publishdate = "2016-00-00"
+draft = "true"
+domains = [""]
+tags = ["", "", ""]
+categories = ["Tutorial"]
++++
+
+### Summary goes here
+
+<!--more-->
+
+## Intro goes here
+
+## The code
+*/
+
+// ## Imports and globals
+package main
